b1c9672a6867175507d4ad3571f7f5310572c3ad
VictorHarbo <vholesen@hotmail.com>
2019-11-29 13:28:29 +0100

    Added some questions to Adela in my test it all out file

--- pdfannots_in_R/Test it all out.R
+++ pdfannots_in_R/Test it all out.R
@@ -5,10 +5,11 @@ test <- system("python pdfannots.py input/test.pdf") #Det her virker!!!
 system("python pdfannots.py input/test.pdf")
 
 # Is there any way to do regular expressions on the output diretly in R so i dont have to go to regex101.com?
-# Is there any way to print the output from the console into a .txt file so that i could automate the ending process a bit more?
+# Is there any way to print the output from the console into a .txt file so that i could automate the ending of process a bit more?
 #     When i'm using either cat("") or capture.output("") it only prints a 0 - can these functions in anyway print characters?
 # In my paper, should i then make a guide for setting up reticulate (Python in R) or just provide usefull links to those slides you've send me and another page i used?
 # I find it really hard to figure out how to do my references - i have used no references in the way i normally would, more like tutorials 
+# Should our final product be located in teh digital methods github repository or in our own - maybe both?
 
 out <- capture.output(system("python pdfannots.py input/test.pdf"))
 print(out)

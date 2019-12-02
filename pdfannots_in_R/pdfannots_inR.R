e5d6058d0ceace14ae8f6f9eb2eca869f7bdabe2
VictorHarbo <vholesen@hotmail.com>
2019-12-02 15:38:34 +0100

    Added the next part of my final script

--- pdfannots_in_R/pdfannots_inR.R
+++ pdfannots_in_R/pdfannots_inR.R
@@ -12,6 +12,33 @@ library("reticulate")
 ### the -p flag shows the progress of the script in the console. the -o output.txt makes a .txt file with the output in it
 system("python pdfannots.py -p -o output.txt input/a_history_of_medieval_heresy_and_inquisition.pdf") 
 
+#From here we will have to do some cleaning of the final result. The .txt file can have some huge white spaces which can be removed using regular expressions
 
+## First we will have to get the data. Please move the output.txt into the messytxts folder at first and then do the following:
+cname <- file.path("./messytxts/")   
+cname   
+dir(cname)
 
+## Get R packages for text mining
+library(tm)
+
+## Lets load texts to R
+docs <- VCorpus(DirSource(cname))   
+summary(docs)
+
+## For details about documents in the corpus, use the inspect(docs) command.
+inspect(docs[1])
+
+# To write out the full text, use writeLines()
+class(docs[1])
+writeLines(as.character(docs[1]))
+
+#This is the regular expression section to use if there are a lot of \t in the plain text file from writeLines(as.character(docs[1]))
+for (j in seq(docs)) {
+  docs[[j]] <- gsub( "\t"," ", docs[[j]]) 
+  docs[[j]] <- gsub( " +"," ", docs[[j]])
+}
+
+#This command saves the changes to the documents
+docs <- tm_map(docs, PlainTextDocument)
 

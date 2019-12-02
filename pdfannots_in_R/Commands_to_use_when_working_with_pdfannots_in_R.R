63a04a39d11466e77d8ac47aa812d1ec3eda0603
VictorHarbo <vholesen@hotmail.com>
2019-12-02 11:26:57 +0100

    Annotated some parts of my script

--- pdfannots_in_R/Commands_to_use_when_working_with_pdfannots_in_R.R
+++ pdfannots_in_R/Commands_to_use_when_working_with_pdfannots_in_R.R
@@ -2,7 +2,8 @@ install.packages("Rtools") #Installs Rtools package
 install.packages("reticulate") #Installs reticulate package
 library("reticulate") #Loads reticulate package - now the exotic animal (python) can live in your home (Rstudio)
 
-repl_python() #Opens python in R
+repl_python() #This command opens the python console and lets you use python commands directly in R
+# To run the pdfannots.py script this commands is not used. See the system() commands
 
 # Different commands comming with the reticulate package 
 py_run_file("FILENAME.PY") #Runs a python file ending in .py - this is the typical form of scripts 
@@ -10,7 +11,7 @@ py_run_string("STRING") #Runs a string in python
 source_python("FILENAME.PY") #This sources a python.py file - does almost the same as py_run_file
 system("python pdfannots.py PATH TO FILE") #This command runs the pdfannots.py script with the path to the .pdf file to run it on
 
-# DENNE COMMAND GØR AT MAN KAN KØRE SCRIPTET MED EN INFILE
+# Commands to run the script with an INFILE and aan output file
 system("python pdfannots.py input/FILENAME") #This command runs the script directly in the console 
 system("python pdfannots.py -p -o testoutput.txt input/INFILE") #This command tuns the script, showing the progress in the console and gives the result in a .txt file called "testoutput.txt"
 system("python pdfannots.py --help") #Brings up the help menu

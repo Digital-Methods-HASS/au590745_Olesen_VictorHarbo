a64ce055c7e0c206c5408bda6e1c126ce9676c66
VictorHarbo <vholesen@hotmail.com>
2019-12-03 10:08:52 +0100

    Added some questions, that i have to figure out

--- pdfannots_in_R/Cleaning_messytxt.R
+++ pdfannots_in_R/Cleaning_messytxt.R
@@ -20,8 +20,10 @@ class(docs[1])
 writeLines(as.character(docs[1]))
 
 #This is the regular expression section to use if there are a lot of \t in the plain text file from writeLines(as.character(docs[1]))
+###I need to make a regular expression, that substitutes ", " with a newline
+### Can the tm-package make an output file as well, instead of just making a change to the corpus
 for (j in seq(docs)) {
-  docs[[j]] <- gsub("\t"," ", docs[[j]])
+  docs[[j]] <- gsub("\t"," ", docs[[j]]) 
 }
 
 #To show the file (or one of the files) afterwards use this command again:
